c950d1344e99b4156a25a117bff626a57c759cab
oscar.santamaria <osantamaria@gmail.com>
2021-11-13 16:44:51 -0600

    Custom Authentication Filter

--- ssc-brewery/src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ ssc-brewery/src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -4,6 +4,7 @@ import guru.sfg.brewery.web.controllers.BaseIT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,6 +26,14 @@ public class BeerRestControllerIT extends BaseIT {
     @Test
     void findBeerByUpc() throws Exception{
         mockMvc.perform(get("/api/v1/beerUpc/0631234200036"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteBeer() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                        .header("Api-Key", "spring")
+                        .header("Api-Secret", "guru"))
                 .andExpect(status().isOk());
     }
 
