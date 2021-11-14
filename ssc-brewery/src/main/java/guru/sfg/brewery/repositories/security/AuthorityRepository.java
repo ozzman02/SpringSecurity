da71af8ca6a9fb30c2ccddda7269e5b4fcd34fa3
oscar.santamaria <osantamaria@gmail.com>
2021-11-14 15:56:36 -0600

    Database authentication - Bootstrap data

--- ssc-brewery/src/main/java/guru/sfg/brewery/repositories/security/AuthorityRepository.java
+++ ssc-brewery/src/main/java/guru/sfg/brewery/repositories/security/AuthorityRepository.java
@@ -4,6 +4,11 @@ import guru.sfg.brewery.domain.security.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/*
+    How does Spring Data JPA manage transactions?
+    Unless you've initialized a transaction in your code, Spring Data JPA will implicitly create a transaction when repository methods are called.
+    This can later cause you issues with lazily loaded references - if you try to access them outside the transactional scope.
+*/
 @Repository
 public interface AuthorityRepository extends JpaRepository<Authority, Integer> {
 }
--- ssc-brewery/src/main/java/guru/sfg/brewery/repositories/security/UserRepository.java
+++ ssc-brewery/src/main/java/guru/sfg/brewery/repositories/security/UserRepository.java
@@ -6,6 +6,11 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/*
+    How does Spring Data JPA manage transactions?
+    Unless you've initialized a transaction in your code, Spring Data JPA will implicitly create a transaction when repository methods are called.
+    This can later cause you issues with lazily loaded references - if you try to access them outside the transactional scope.
+*/
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
