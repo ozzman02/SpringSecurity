58ecf204ac5c0c4c4042701639b736d031cbac7e
oscar.santamaria <osantamaria@gmail.com>
2021-12-25 15:22:50 -0600

    Persistence of authentication login success and login failed events

--- ssc-brewery/src/main/java/guru/sfg/brewery/security/listeners/AuthenticationSuccessListener.java
+++ ssc-brewery/src/main/java/guru/sfg/brewery/security/listeners/AuthenticationSuccessListener.java
@@ -1,6 +1,9 @@
 package guru.sfg.brewery.security.listeners;
 
+import guru.sfg.brewery.domain.security.LoginSuccess;
 import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.LoginSuccessRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,22 +12,30 @@ import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class AuthenticationSuccessListener {
+
+    private final LoginSuccessRepository loginSuccessRepository;
 
     @EventListener
     public void listen(AuthenticationSuccessEvent authenticationSuccessEvent) {
         log.debug("User logged in okay");
         if (authenticationSuccessEvent.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            LoginSuccess.LoginSuccessBuilder builder = LoginSuccess.builder();
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authenticationSuccessEvent.getSource();
             if(token.getPrincipal() instanceof User){
                 User user = (User) token.getPrincipal();
+                builder.user(user);
                 log.debug("User name logged in: " + user.getUsername() );
             }
             if (token.getDetails() instanceof WebAuthenticationDetails){
                 WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
                 log.debug("Source IP: " + details.getRemoteAddress());
+                builder.sourceIp(details.getRemoteAddress());
             }
+            LoginSuccess loginSuccess = loginSuccessRepository.save(builder.build());
+            log.debug("Login Success saved. Id: " + loginSuccess.getId());
         }
     }
 
