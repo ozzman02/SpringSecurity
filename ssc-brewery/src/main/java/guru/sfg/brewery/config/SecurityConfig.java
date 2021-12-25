f5b1f56ce8082555e299ef486918c63534695c1b
oscar.santamaria <osantamaria@gmail.com>
2021-12-24 21:39:17 -0600

    Login form - Login and logout messages

--- ssc-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ ssc-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -62,10 +62,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .loginProcessingUrl("/login")
                         .loginPage("/").permitAll()
                         .successForwardUrl("/")
-                        .defaultSuccessUrl("/"))
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/?error"))
                 .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
-                        .logoutSuccessUrl("/").permitAll())
+                        .logoutSuccessUrl("/?logout").permitAll())
                 .httpBasic()
                 .and().csrf().ignoringAntMatchers("/h2-console/**", "/api/**");
 
