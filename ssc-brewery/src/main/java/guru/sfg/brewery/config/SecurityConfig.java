0d7a1cbf05a204454481b3d936264a2ecb30666b
oscar.santamaria <osantamaria@gmail.com>
2021-12-26 14:39:20 -0600

    CORS

--- ssc-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ ssc-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package guru.sfg.brewery.config;
 
-import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 import guru.sfg.brewery.security.google.Google2faFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.session.SessionManagementFilter;
@@ -34,16 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new SecurityEvaluationContextExtension();
     }
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .addFilterBefore(google2faFilter, SessionManagementFilter.class);
         http
+                .cors().and()
                 .authorizeRequests(authorize -> {
                     authorize
                             .antMatchers("/h2-console/**").permitAll()
--- ssc-brewery/src/test/java/guru/sfg/brewery/web/controllers/IndexControllerIT.java
+++ ssc-brewery/src/test/java/guru/sfg/brewery/web/controllers/IndexControllerIT.java
@@ -1,14 +1,17 @@
 package guru.sfg.brewery.web.controllers;
 
+import com.warrenstrange.googleauth.GoogleAuthenticator;
 import guru.sfg.brewery.repositories.BeerInventoryRepository;
 import guru.sfg.brewery.repositories.BeerRepository;
 import guru.sfg.brewery.repositories.CustomerRepository;
+import guru.sfg.brewery.repositories.security.UserRepository;
 import guru.sfg.brewery.services.BeerOrderService;
 import guru.sfg.brewery.services.BeerService;
 import guru.sfg.brewery.services.BreweryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,10 +37,18 @@ public class IndexControllerIT extends BaseIT {
     @MockBean
     BeerOrderService beerOrderService;
 
+    @MockBean
+    PersistentTokenRepository persistentTokenRepository;
+
+    @MockBean
+    UserRepository userRepository;
+
+    @MockBean
+    GoogleAuthenticator googleAuthenticator;
+
     @Test
     void testGetIndexSlash() throws Exception{
-        mockMvc.perform(get("/" ))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/" )).andExpect(status().isOk());
     }
 
 }
